7e30fa6432e7daa7b9059b7b1bea80f44d002c75
Tempest96 <eldorarempest@gmail.com>
2021-03-10 12:55:39 +0200

    Added last functions to gulp, and do some tests..

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ const sass = require('gulp-sass');
 const autopref = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
-
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+const del = require('del')
 
 const html = () => {
   return gulp
@@ -24,3 +26,41 @@ const styles = () => {
     .pipe(gulp.dest('build/css'))
 }
 
+
+const images = () => {
+  return gulp
+    .src('src/images/*.*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/images'))
+}
+
+
+const server = () => {
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    },
+    notify: false
+  });
+  browserSync.watch('build',browserSync.reload)
+}
+
+const deleteBuild = (cb) => {
+  return del('build/**/*.*')
+    .then(() => {
+      cb();
+    })
+
+}
+
+const watch = () => {
+  gulp.watch('src/pug/*.pug',html);
+  gulp.watch('src/styles/*.scss',styles);
+  gulp.watch('src/images/*.*',images);
+}
+
+exports.default = series(
+  deleteBuild,
+  parallel(html,styles,images),
+  parallel(watch,server)
+)
