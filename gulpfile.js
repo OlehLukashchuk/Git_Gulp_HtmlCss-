7cfdf2163e65d2e3b8c735b03df4a13d259d7490
Tempest96 <eldorarempest@gmail.com>
2021-03-10 12:21:44 +0200

    Created structure for html with pug , added gulp-pug and created pug to html converter

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
 const gulp = require('gulp');
+const {series , parallel} = gulp;
+const pug = require('gulp-pug');
 
-
+const html = () => {
+  return gulp
+    .src("src/pug/*.pug")
+    .pipe(pug())
+    .pipe(gulp.dest('build'))
+}
