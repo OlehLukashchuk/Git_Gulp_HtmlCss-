456663c708bc39ae3fb35b5b37127dd8614761a0
Tempest96 <eldorarempest@gmail.com>
2021-03-10 12:36:46 +0200

    Added all scss code

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp');
 const {series , parallel} = gulp;
 const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const autopref = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+
 
 const html = () => {
   return gulp
@@ -8,3 +13,14 @@ const html = () => {
     .pipe(pug())
     .pipe(gulp.dest('build'))
 }
+
+const styles = () => {
+  return gulp
+    .src('src/styles/*.scss')
+    .pipe(sass().on('err',sass.logError))
+    .pipe(autopref())
+    .pipe(cssnano())
+    .pipe(rename({suffix : '.min'}))
+    .pipe(gulp.dest('build/css'))
+}
+
